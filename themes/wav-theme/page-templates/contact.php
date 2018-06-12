0241e04a6393bed322cde851f4e14c9b5e6ba171
im-chang <mchang0718@gmail.com>
2018-06-12 16:42:36 -0700

    Create Contact Page

---
+++ themes/wav-theme/page-templates/contact.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Contact
+ * 
+ * The template for displaying all pages.
+ *
+ * @package WAV_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
