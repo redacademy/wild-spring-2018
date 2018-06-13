7e97e411ea8657152048d4167c878fb95892cfaf
im-chang <mchang0718@gmail.com>
2018-06-13 10:33:51 -0700

    Create Contact Form page

--- themes/wav-theme/page-templates/contact.php
+++ themes/wav-theme/page-templates/contact.php
@@ -12,11 +12,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
