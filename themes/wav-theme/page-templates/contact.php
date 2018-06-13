291bf1c9e103e86f8c688d9a7aa98147cfa64d60
im-chang <mchang0718@gmail.com>
2018-06-13 12:18:31 -0700

    add hero image for contact page

--- themes/wav-theme/page-templates/contact.php
+++ themes/wav-theme/page-templates/contact.php
@@ -9,9 +9,16 @@
 
 get_header(); ?>
 
+<img src="<?php echo CFS()->get( 'contact_img' ); ?>">
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
