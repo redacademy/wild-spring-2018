fa873d5d9332a699d01de11b7e4cfae98a13a426
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-06-13 23:23:49 -0700

    add front-page.js and script for flickity for front-page. Not finished

--- themes/wav-theme/functions.php
+++ themes/wav-theme/functions.php
@@ -90,6 +90,8 @@ function wav_starter_scripts() {
 
 	wp_enqueue_script( 'wav-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
+	wp_enqueue_script( 'front-page', get_template_directory_uri() . '/build/js/front-page.min.js', array('jquery'), '20180613', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
