e1ba92e1d2d82186c7722e320f6071a746d0875d
Pamela Chong <pjchong91@gmail.com>
2018-06-14 19:53:53 -0700

    Styled carousel for team members

--- themes/wav-theme/page-templates/about.php
+++ themes/wav-theme/page-templates/about.php
@@ -61,29 +61,71 @@ get_header(); ?>
             <div class="anchor" id="teamJump"></div>
             <section class="theTeam">
                 <h1>The Team</h1>
-                    <div class="teamMembers">
+                    <div class="teamMembers carousel">
 
                         <?php
-                            $args = array( 'post_type' => 'person', 'category_name' => 'team', 'order' => 'ASC');
+                            $args = array( 'post_type' => 'person', 'category_name' => 'team', 'order' => 'ASC', 'numberposts'=>-1);
                             $team_posts = get_posts( $args ); // returns an array of posts
                         ?>
 
                         <?php foreach ( $team_posts as $post ) : setup_postdata( $post ); ?>
                     
-                            <div class="teamMember">
+                            <div class="teamMember carousel-cell">
                                 <header class="entry-header">
                                     <?php if ( has_post_thumbnail() ) : ?>
-                                        <?php the_post_thumbnail( 'large' ); ?>
+                                    <a href="<?php echo esc_url( get_permalink()) ?>" alt="Team Image"><?php the_post_thumbnail( 'large' ); ?></a>
                                     <?php endif; ?>
 
                                     <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
                                 </header>
+                                
      
                             </div><!--end of teamMember-->
                             <?php endforeach; wp_reset_postdata(); ?>
                     </div><!--end of teamMembers-->
+                    <a class="hidden">See More</a>
+                </section>
 
-            </section>
+          
+
+<div class="anchor" id="collaboratorsJump"></div>
+<h1>Collaborators</h1>
+<section class="container-holder">
+            <div class="container">
+                <h1 class="container-header">Inhabitent Journal</h1>
+                <div class="inhabitent-journal listed-posts">
+
+            <?php
+                $args = array( 'post_type' => 'person', 'category_name' => 'collaborator', 'order' => 'ASC', 'numberposts' => '4' );
+                $collaborators = get_posts( $args ); // returns an array of posts
+            ?>
+
+            <?php foreach ( $collaborators as $post ) : setup_postdata( $post ); ?>
+            <div class="ijbox">
+                <header class="entry-header">
+                    <?php if ( has_post_thumbnail() ) : ?>
+                        <?php the_post_thumbnail( 'large' ); ?>
+                    <?php endif; ?>
+                </header>
+
+                <div class="entry-meta solid-border">
+                    <div class="journal-meta">
+                        
+                        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                    </div> <!-- end of journal-meta-->
+
+                    <div class="read-more">
+                        <a href="<?php echo esc_url(get_permalink())?>" class="button-border">Read Entry </a>
+                    </div><!--end of read-more-->
+
+                </div><!--end of entry-meta-->
+            </div><!--end of ijbox-->
+
+                    <?php endforeach; wp_reset_postdata(); ?>
+                </div> <!--end of inhabitent-journal-->
+
+            </div><!--end of container-->
+        </section>
             
 		</main><!-- #main -->
 	</div><!-- #primary -->
