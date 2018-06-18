300ab45931eccbed567584754f69f81d43bcfe21
im-chang <mchang0718@gmail.com>
2018-06-18 10:51:22 -0700

    add faq accordion

--- themes/wav-theme/faq/faq.php
+++ themes/wav-theme/faq/faq.php
@@ -1,20 +1,30 @@
-<?php add_shortcode('faq', function() {
- 
- $posts = get_posts(array(  //Get the FAQ Custom Post Type
-     'numberposts' => 10,
-     'orderby' => 'menu_order',
-     'order' => 'ASC',
-     'post_type' => 'faq',
- ));
+<?php
 
- $faq  = '<div id="wptuts-accordion">'; //Open the container
- foreach ( $posts as $post ) { // Generate the markup for each Question
-     $faq .= sprintf(('<h3><a href="">%1$s</a></h3><div>%2$s</div>'),
-         $post->post_title,
-         wpautop($post->post_content)
-     );
- }
- $faq .= '</div>'; //Close the container
-
- return $faq; //Return the HTML.
-});
+add_shortcode( 'faq', array('FAQs', 'shortcode_callback') );
+class FAQs {
+	
+	protected static $scripts_loaded = FALSE;
+	
+	public static function shortcode_callback( $atts, $content='') {
+		if(!self::$scripts_loaded)
+			self::enqueue_files();
+		
+		extract( shortcode_atts(array('title' => ''), $atts) );
+		
+		$html;
+		$html = '<div class="faq-item">';
+		$html   .= '<a class="question">' . $title . '</a>';
+		$html	.= '<div class="answer">'. $content . '</div>';
+		$html .='</div>';
+		
+		return $html;
+		
+	}
+	
+	protected static function enqueue_files() {
+		wp_enqueue_script('jquery');
+			
+		
+	}
+}
+?>
