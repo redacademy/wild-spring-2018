cd65e2e425c506a64a1d5cea7fe0df7d63a81213
im-chang <mchang0718@gmail.com>
2018-06-18 00:14:08 -0700

    add css for mobile and desktop on contact page

--- themes/wav-theme/page-templates/contact.php
+++ themes/wav-theme/page-templates/contact.php
@@ -12,29 +12,34 @@ get_header(); ?>
 <div class="top" id="top">
 		<main id="main" class="site-main" role="main">
 			<img src="<?php echo CFS()->get( 'contact_image' ); ?>">
-			<div class="contact-top">
-				<h2 class="contactus"> contact us </h2>
-				<div class="contact-button1">
-					<a href="#faq">faq</a>
-			</div>
-			</div>
-			<aside id="sidebar-2">
-				<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-2') ) ?>
-			</aside>
-
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php endwhile; // End of the loop. ?>
-
-				<section class="faq" id="faq">
-				</section>
-
-				<div class="contact-button2">
-					<a href="#top">Back To Top</a>
+			<div class="contact-main">
+				<div class="contactandfaq">
+					<div class="contact-top">
+						<h2 class="contactus"> contact us </h2>
+						<div class="contact-button1">
+							<a href="#faq">faq</a>
+					</div>
+					</div>
+					<aside class="sidebar-2" id="sidebar-2">
+						<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-2') ) ?>
+					</aside>
 				</div>
+				<div class="contact-submit">
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+					<?php endwhile; // End of the loop. ?>
+
+					<section class="faq" id="faq">
+					</section>
+
+					<div class="contact-button2">
+						<a href="#top">Back To Top</a>
+					</div>
+				</div>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
