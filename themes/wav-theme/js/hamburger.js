307e8ef43f33c72b8e4ad00b009bdeb897c4a475
im-chang <mchang0718@gmail.com>
2018-06-19 12:07:52 -0700

    nav styles

--- themes/wav-theme/js/hamburger.js
+++ themes/wav-theme/js/hamburger.js
@@ -2,11 +2,13 @@
     var $menuToggle = $('.menu-toggle');
     var $mainNavigation = $('.main-navigation');
     var $hamburgerMenu = $('.hamburger');
+    var $siteheader = $('.site-header');
   
     $menuToggle.on('click', function(evt) {
       evt.preventDefault();
       $mainNavigation.toggleClass('toggled');
       $hamburgerMenu.toggleClass('is-active');
+      $siteheader.addClass('head2');
     });
   
   })(jQuery);
