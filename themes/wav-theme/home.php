6978e00d95344f099908463c5888ca206a4ebc70
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-06-19 10:12:35 -0700

    editing the home and sigle page

--- themes/wav-theme/home.php
+++ themes/wav-theme/home.php
@@ -21,7 +21,33 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php if ( has_post_thumbnail() ) : ?>
+                            <?php the_post_thumbnail( 'large' ); ?>
+                        <?php endif; ?>
+
+                        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+                        <?php if ( 'post' === get_post_type() ) : ?>
+                        <div class="entry-meta">
+                            <?php WAV_Starter_posted_on(); ?>
+                        </div><!-- .entry-meta -->
+                        <?php endif; ?>
+                    </header><!-- .entry-header -->
+
+                    <div class="entry-content">
+                        <?php the_excerpt(); ?>
+                    </div><!-- .entry-content -->
+                </article><!-- #post-## -->
+
+				<?php									
+                 $tags_list = get_the_tag_list( '', esc_html( ', ' ) );
+                if ( $tags_list ) {
+                printf( '<span class="tags-links">' . esc_html( 'Tagged &rarr; %1$s' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+                }
+                ?>
+                <a href="<?php the_permalink(); ?>"class="journal-button">See Details</a>            
 
 			<?php endwhile; ?>
 
