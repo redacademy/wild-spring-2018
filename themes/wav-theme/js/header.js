a4f06e29fddf834ea5f818f90de4b80656d30330
im-chang <mchang0718@gmail.com>
2018-06-21 16:09:12 -0700

    desktop header css

--- themes/wav-theme/js/header.js
+++ themes/wav-theme/js/header.js
@@ -7,23 +7,22 @@ if($('body').hasClass('home') || $('body').hasClass('page-template-contact')||$(
 
 $(window).scroll(function(){
     var yPos=$(window).scrollTop();
+    var xPos=$(window).scrollTop();
 
     if (yPos<bannerHeight){
         $('.site-header').addClass('light-header').removeClass('dark-header');
         $('.logo-wav2').css('display','none');
         $('.logo-wav1').css('display','block');
-        $('.menu-main-menu-container a').css('color', 'white');
         $('.light-header').css('background-color', 'transparent');
-        $('.menu a:hover').css('border-bottom', '2px solid #fff');
+        
     }
     
     else{
         $('.site-header').removeClass('light-header').addClass('dark-header');
         $('.logo-wav2').css('display','block');
         $('.logo-wav1').css('display','none');
-        $('.menu-main-menu-container a').css('color', 'black');
         $('.dark-header').css('background-color', 'hsla(0,0%,100%,.85)');
-        $('.menu a:hover').css('border-bottom', '2px solid black');
+   
     }
 });
 
@@ -32,9 +31,10 @@ $(window).scroll(function(){
 //this will default load the dark-banner on pages that are not 'about.php' or 'front-page'.php
 else{
     $('.site-header').removeClass('light-header').addClass('dark-header');
-    $('.dark-logo').css('display','block');
-    $('.logo').css('display','none');
-    $('.offset-placeholder').addClass('header-offset');
+    $('.logo-wav2').css('display','block');
+    $('.logo-wav1').css('display','none');
+    $('.dark-header').css('background-color', 'hsla(0,0%,100%,.85)');
+  
 }
 })(jQuery);
 
