1df01f15501e3f41b385914061a0b27af71a4126
im-chang <mchang0718@gmail.com>
2018-06-25 20:37:42 -0700

    add faq to enqueue

--- themes/wav-theme/tribe-events/community/edit-event.php
+++ themes/wav-theme/tribe-events/community/edit-event.php
@@ -57,28 +57,58 @@ if ( ! isset( $tribe_event_id ) ) {
 </form>
 
 <div class="faq-event-submission">
-[faq title="Who Can Submit an Event/Lesson?"]Vancouver community-based organizations that offer outdoor education experiences are invited to “showcase” what they are currently doing by contributing an event to the festival. Potential hosts may be, but not limited to, non-profits, for-profits, and in/formal educational organizations (including those who may not see themselves as being just outdoor-based). Teachers are also invited to submit a lesson to WAV, showcasing the teacher they are doing outdoors.[/faq]
+	<div class="Accordian">
+		<div class="question">
+		Who Can Submit an Event/Lesson?
+		</div>
+		<div class="answer">	
+		Vancouver community-based organizations that offer outdoor education experiences are invited to “showcase” what they are currently doing by contributing an event to the festival. Potential hosts may be, but not limited to, non-profits, for-profits, and in/formal educational organizations (including those who may not see themselves as being just outdoor-based).  Teachers are also invited to submit a lesson to WAV, showcasing the teacher they are doing outdoors.
+		</div>
+		<div class="question">
+		Who Will Attend the event/lesson?
+		</div>
+		<div class="answer">	
+		The weekday events will focus on schools, teachers, community-based organizations, and students, while the weekend events will be directed towards families and community-members at large.  Most school lessons are closed events, and not open to the general public.  Events may be open or have a registration.  Check each event/lesson for details.
+		</div>
+		<div class="question">
+		Looking for Event/Lesson Ideas?
+		</div>
+		<div class="answer">	
+		Take a look at the great events our 2015 Event Hosts and 2016 Events Hosts offered.
+		</div>
+		<div class="question">
+		Event/Lesson Role
+		</div>
+		<div class="answer">	
+		Event hosts will determine the “event” they wish to contribute to the festival (stand-up paddle boarding, gardening, cycling, art, music, etc). All aspects of the event are organized by each event host including: the date and time of the event, equipment, safety, supervision, insurance, and registration of participants. In addition:
 
-[faq title="Who Will Attend the event/lesson?"]The weekday events will focus on schools, teachers, community-based organizations, and students, while the weekend events will be directed towards families and community-members at large. Most school lessons are closed events, and not open to the general public. Events may be open or have a registration. Check each event/lesson for details.[/faq]
+		The event will be located in Vancouver
+		There will be no fee for participants
+		There will be an educational component that invites participants to take something away that they can enact elsewhere (school, home, community-centre, etc.) and share their experience with others
+		Lessons will be offered by teachers/educators.  They will be closed events that only the teacher and students participate in.  Different classes many join up for a lesson, or even different schools and grade levels may collaborate for an outdoor lesson.  Outdoor lessons are a chance to share what you are currently doing outdoors with other teachers/educators.
+		</div>
+		<div class="question">
+		Our Part
+		</div>
+		<div class="answer">
+		Wild About Vancouver will offer advertising and promotion of your event in addition to an event schedule and map of the festival to assist participants in choosing events to attend. WAV will also hold a preparation workshop for event providers in the weeks leading up to the festival. After the event, WAV will help collect data from the event so we can all learn from the experience.
+		For lessons, WAV will share your outdoor teaching with other teachers/educators to help inspire them to take their teaching outdoors and offer some ideas about the range of learning that can happen outdoors.
+		</div>
+		<div class="question">
+		I Need Help Planning My Event/Lesson
+		</div>
+		<div class="answer">	
+		Check out this helpful Wild About Vancouver Event Host Guide.
+		</div>
+		<div class="question">
+		I Need Funding for My Event/Lesson
+		</div>
+		<div class="answer">	
+		Groups and/or individuals wishing to offer an event in the WAV Festival may apply for funding to help with costs associated with their event. There is no guarantee that WAV will be able to find a sponsor, but we will try!
 
-[faq title="Looking for Event/Lesson Ideas?"]Take a look at the great events our 2015 Event Hosts and 2016 Events Hosts offered.[/faq]
-
-[faq title="Event/Lesson Role"]Event hosts will determine the “event” they wish to contribute to the festival (stand-up paddle boarding, gardening, cycling, art, music, etc). All aspects of the event are organized by each event host including: the date and time of the event, equipment, safety, supervision, insurance, and registration of participants. In addition:
-
-The event will be located in Vancouver
-There will be no fee for participants
-There will be an educational component that invites participants to take something away that they can enact elsewhere (school, home, community-centre, etc.) and share their experience with others
-Lessons will be offered by teachers/educators. They will be closed events that only the teacher and students participate in. Different classes many join up for a lesson, or even different schools and grade levels may collaborate for an outdoor lesson. Outdoor lessons are a chance to share what you are currently doing outdoors with other teachers/educators.[/faq]
-
-[faq title="Our Part"]Wild About Vancouver will offer advertising and promotion of your event in addition to an event schedule and map of the festival to assist participants in choosing events to attend. WAV will also hold a preparation workshop for event providers in the weeks leading up to the festival. After the event, WAV will help collect data from the event so we can all learn from the experience.
-For lessons, WAV will share your outdoor teaching with other teachers/educators to help inspire them to take their teaching outdoors and offer some ideas about the range of learning that can happen outdoors.[/faq]
-
-[faq title="I Need Help Planning My Event/Lesson"]Check out this helpful Wild About Vancouver Event Host Guide.[/faq]
-
-[faq title="I Need Funding for My Event/Lesson"]Groups and/or individuals wishing to offer an event in the WAV Festival may apply for funding to help with costs associated with their event. There is no guarantee that WAV will be able to find a sponsor, but we will try!
-
-Looking for funding for your event? Check out Go Grants or the City of Vancouver’s Greenest City Neighbourhood Small Grants.[/faq]
-
+		Looking for funding for your event?  Check out Go Grants  or the City of Vancouver’s Greenest City Neighbourhood Small Grants.
+		</div>
+	</div>
 </div>
 
 <?php do_action( 'tribe_events_community_form_after_template', $tribe_event_id ); ?>
