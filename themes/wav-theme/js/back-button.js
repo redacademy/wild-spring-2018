b17be7d7dfffea9d3118575f6bd2e55aa0f72b62
im-chang <mchang0718@gmail.com>
2018-06-26 11:26:47 -0700

    back button only for single mobile

--- themes/wav-theme/js/back-button.js
+++ themes/wav-theme/js/back-button.js
@@ -1,5 +1,6 @@
 (function($) { 
-    if($('body').hasClass('single')){
+    var width = $('body').width();
+    if($('body').hasClass('single') && width < 1060){
         $('.logo a').css('display','none'),
         $('.back-button').css('display', 'block'),
         $('.main-navigation ul').css('top', '3rem');
