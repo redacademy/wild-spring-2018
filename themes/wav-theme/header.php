a3e62990071697424dd6b3bb7fd004329427f64a
im-chang <mchang0718@gmail.com>
2018-06-26 12:05:07 -0700

    tablet hamburger

--- themes/wav-theme/header.php
+++ themes/wav-theme/header.php
@@ -34,6 +34,7 @@
 						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 						<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
+				<p class="mobile-title"> Wild About Vancouver </p>
 
 				<div class="menu-toggle">
 
